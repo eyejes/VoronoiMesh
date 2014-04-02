fb00daff7da8cad1f54d20103c9a48d3f254733c
Udayan <uday1489@gmail.com>
2014-04-01 21:37:16 -0400

    Added random walk algorithm.

--- src/com/uday/processing/VoronoiMeshProject.java
+++ src/com/uday/processing/VoronoiMeshProject.java
@@ -17,11 +17,14 @@ public class VoronoiMeshProject extends PApplet {
 	float[][] points = new float[3][2];
 	Voronoi myVoronoi;
 	int pointCounter;
-	float easing = (float)0.1;
+	float easing = (float) 0.005;
 	float targetX = 0;
 	float targetY = 0;
 	float threshold = 25;
-	
+	float dx;
+	float dy;
+	boolean newPointAdded;
+
 	public void setup() {
 
 		pointsArray.add((float) 200);
@@ -31,14 +34,40 @@ public class VoronoiMeshProject extends PApplet {
 		pointsArray.add((float) 320);
 		pointsArray.add((float) 113);
 
-		pointCounter = pointsArray.toArray().length/2;
+		pointCounter = pointsArray.toArray().length / 2;
 		println(pointCounter);
 		size(displayWidth, displayHeight);
+
+		newPointAdded = false;
 	}
 
 	public void draw() {
 
 		points = new float[(pointsArray.size() / 2)][2];
+		
+		if( (newPointAdded || (pow(targetX - points[pointCounter - 1][0],2))+(pow(targetY - points[pointCounter - 1][1],2)) < threshold) ){
+			int randomPoint = pointCounter - 1;
+			while(randomPoint == pointCounter - 1){
+				randomPoint = (int)random(pointCounter - 1);
+			}
+			targetX = pointsArray.get(2*randomPoint);
+			targetY = pointsArray.get(2*randomPoint + 1);
+			dx =  targetX - pointsArray.get(2*(pointCounter - 1));
+			dy = targetY - pointsArray.get(2*(pointCounter - 1) + 1);
+			println(dx,dy, newPointAdded);
+			newPointAdded = false;
+		}
+		
+		else{
+			float px = pointsArray.get(2*(pointCounter - 1));
+			float py = pointsArray.get(2*(pointCounter - 1) + 1);
+			
+			pointsArray.remove(pointsArray.size() - 1);
+			pointsArray.remove(pointsArray.size() - 1);
+			
+			pointsArray.add(pointsArray.size(), px + easing*dx);
+			pointsArray.add(pointsArray.size(), py + easing*dy);
+		}
 		
 		for (int i = 0; i < pointsArray.size(); i = i + 2) {
 			points[i / 2][0] = pointsArray.get(i);
@@ -67,7 +96,8 @@ public class VoronoiMeshProject extends PApplet {
 	public void mousePressed() {
 		boolean pointAlreadyInList = false;
 		for (int i = 0; i < pointCounter; i++) {
-			if (points[i][0] == (float)mouseX && points[i][1] == (float)mouseY) {
+			if (points[i][0] == (float) mouseX
+					&& points[i][1] == (float) mouseY) {
 				pointAlreadyInList = true;
 			}
 		}
@@ -75,7 +105,9 @@ public class VoronoiMeshProject extends PApplet {
 		if (pointAlreadyInList == false) {
 			pointsArray.add(pointsArray.size(), (float) mouseX);
 			pointsArray.add(pointsArray.size(), (float) mouseY);
-			pointCounter = pointsArray.size()/2;
+			pointCounter = pointsArray.size() / 2;
+
+			newPointAdded = true;
 		}
 
 	}
@@ -84,7 +116,8 @@ public class VoronoiMeshProject extends PApplet {
 
 		boolean pointAlreadyInList = false;
 		for (int i = 0; i < pointCounter / 2; i++) {
-			if (points[i][0] == (float)mouseX && points[i][1] == (float)mouseY) {
+			if (points[i][0] == (float) mouseX
+					&& points[i][1] == (float) mouseY) {
 				println("point already in array");
 				pointAlreadyInList = true;
 			}
@@ -97,7 +130,7 @@ public class VoronoiMeshProject extends PApplet {
 			pointsArray.add(pointsArray.size(), (float) mouseX);
 			pointsArray.add(pointsArray.size(), (float) mouseY);
 
-			pointCounter = pointsArray.size()/2;
+			pointCounter = pointsArray.size() / 2;
 		}
 
 	}
