dcef98474aa354556ccfad8a8860fb5fbcd80aab
Udayan <uday1489@gmail.com>
2014-04-18 03:46:18 -0400

    Corrected the issue with random walk. Added heartbeat.

--- src/com/uday/processing/VoronoiMeshProject.java
+++ src/com/uday/processing/VoronoiMeshProject.java
@@ -17,13 +17,17 @@ public class VoronoiMeshProject extends PApplet {
 	float[][] points = new float[3][2];
 	Voronoi myVoronoi;
 	int pointCounter;
-	float easing = (float) 0.005;
+	float easing = (float) 0.0025;
 	float targetX = 0;
 	float targetY = 0;
-	float threshold = 25;
+	float threshold = 200;
 	float dx;
 	float dy;
 	boolean newPointAdded;
+	float px;
+	float py;
+	int heartBeat = 0;
+	boolean heartBeatUp = true;
 
 	public void setup() {
 
@@ -44,31 +48,50 @@ public class VoronoiMeshProject extends PApplet {
 	public void draw() {
 
 		points = new float[(pointsArray.size() / 2)][2];
-		
-		if( (newPointAdded || (pow(targetX - points[pointCounter - 1][0],2))+(pow(targetY - points[pointCounter - 1][1],2)) < threshold) ){
+
+		if ((newPointAdded || (pow(
+				targetX - pointsArray.get(2 * (pointCounter - 1)), 2))
+				+ (pow(targetY - pointsArray.get(2 * (pointCounter - 1) + 1), 2)) < threshold)) {
+			println("hit target");
 			int randomPoint = pointCounter - 1;
-			while(randomPoint == pointCounter - 1){
-				randomPoint = (int)random(pointCounter - 1);
+			while (randomPoint == pointCounter - 1) {
+				randomPoint = (int) (random(67867) % (pointCounter - 1));
+				println(randomPoint);
 			}
-			targetX = pointsArray.get(2*randomPoint);
-			targetY = pointsArray.get(2*randomPoint + 1);
-			dx =  targetX - pointsArray.get(2*(pointCounter - 1));
-			dy = targetY - pointsArray.get(2*(pointCounter - 1) + 1);
-			println(dx,dy, newPointAdded);
+			targetX = pointsArray.get(2 * randomPoint);
+			targetY = pointsArray.get(2 * randomPoint + 1);
+			dx = targetX - pointsArray.get(2 * (pointCounter - 1));
+			dy = targetY - pointsArray.get(2 * (pointCounter - 1) + 1);
+			println(targetX, targetY, newPointAdded);
 			newPointAdded = false;
 		}
-		
-		else{
-			float px = pointsArray.get(2*(pointCounter - 1));
-			float py = pointsArray.get(2*(pointCounter - 1) + 1);
-			
+
+		else {
+			px = pointsArray.get(2 * (pointCounter - 1));
+			py = pointsArray.get(2 * (pointCounter - 1) + 1);
+
+			// if (px < 5 || px > displayWidth - 5 || py < 5
+			// || py > displayHeight - 5) {
+			// newPointAdded = true;
+			// }
+
 			pointsArray.remove(pointsArray.size() - 1);
 			pointsArray.remove(pointsArray.size() - 1);
-			
-			pointsArray.add(pointsArray.size(), px + easing*dx);
-			pointsArray.add(pointsArray.size(), py + easing*dy);
+
+			dx = targetX - px;
+			dy = targetY - py;
+			// if (abs(dx) > 1) {
+			px += dx * easing;
+			// }
+
+			// if (abs(dy) > 1) {
+			py += dy * easing;
+			// }
+
+			pointsArray.add(pointsArray.size(), px);
+			pointsArray.add(pointsArray.size(), py);
 		}
-		
+
 		for (int i = 0; i < pointsArray.size(); i = i + 2) {
 			points[i / 2][0] = pointsArray.get(i);
 			points[i / 2][1] = pointsArray.get(i + 1);
@@ -87,8 +110,51 @@ public class VoronoiMeshProject extends PApplet {
 				fill(255);
 			}
 			myRegions[i].draw(this); // draw this shape
+			for (int j = 0; j < points.length; j++) {
+				float ellipseSizeX = 15;
+				float ellipseSizeY = 15;
+
+				if (j == pointCounter - 1) {
+
+					if (heartBeatUp && heartBeat < 100) {
+						heartBeat++;
+					}
+
+					else if (!heartBeatUp && heartBeat > 0) {
+						heartBeat--;
+					}
+
+					else {
+						if (heartBeatUp) {
+							heartBeatUp = false;
+							heartBeat = 100;
+						}
+
+						else {
+							heartBeatUp = true;
+							heartBeat = 0;
+						}
+					}
+
+					stroke(0xFF, 0x77, 0x6D);
+					fill(0xE8, 0x2C, 0x0C);
+					ellipseSizeX = 20 + heartBeat / 8;
+					ellipseSizeY = 20 + heartBeat / 8;
+
+					ellipse(points[j][0], points[j][1], ellipseSizeX,
+							ellipseSizeY);
+				} else {
+					strokeWeight(5);
+					ellipse(points[j][0], points[j][1], ellipseSizeX,
+							ellipseSizeY);
+				}
+
+			}
 		}
 
+		stroke(255);
+		textSize(30);
+		text("px: " + px, 20, 20);
 		// mouseMoved() ;
 
 	}
